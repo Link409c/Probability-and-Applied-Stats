57fff6246baecdc9dfae9d4d99628cb20fa2ded4
Christian Simpson <66807809+Link409c@users.noreply.github.com>
2023-10-23 09:09:24 -0400

    Create Plotter.java

---
+++ src/Plotter.java
@@ -0,0 +1,22 @@
+/*******************************
+* Plotter takes a value and plots a
+* number of points using a defined
+* function. The program then outputs
+* these values to a .csv file users
+* can view results in.
+*
+* Author: Christian Simpson
+* Version: 10/23/23
+********************************/
+
+public class Plotter(){
+  
+  //function method
+  //plot method
+  //output method
+
+  //number of points to plot
+  private int pointCount;
+  //data points to output to the csv
+  private double[] outputPoints;
+}
