8a78cb5e57bd404ac6f194060eafa5dc3e33b4e8
Christian Simpson <66807809+Link409c@users.noreply.github.com>
2023-10-23 09:16:46 -0400

    Create Salter.java

---
+++ src/Salter.java
@@ -0,0 +1,15 @@
+/**
+* Salter takes a csv file input and "salts"
+* the data by adding or subtracting from
+* each value to change the plot. 
+*
+**/
+
+public class Salter(){
+  //method to input the data
+  //method to dalt each data point
+  //method to output the data
+  
+  //the plot data created with salt
+  private double[] plotData;
+}
