eb6a384275cdd2b980bab1344d1dfa3ccff761dc
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-10-31 00:07:15 -0400

    Monty Hall: Coded most of program and ran initial tests. My initial output had the probabilities of the choices backwards, so I began debugging and somehow made it even worse. Will need more time to adjust conditional statements to correctly eliminate doors as they are chosen by the user and the "host". Screenshot included.

--- src/Mini_Coding_Projects/Monty_Hall_DealNoDeal/MonteHall.java
+++ src/Mini_Coding_Projects/Monty_Hall_DealNoDeal/MonteHall.java
@@ -1,8 +1,12 @@
 package Mini_Coding_Projects.Monty_Hall_DealNoDeal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 /**
  * The Monte Hall Problem:
- *
  * (reference Pg. 34 2.20 for this assignment.)
  */
 public class MonteHall {
@@ -15,7 +19,60 @@ public class MonteHall {
      * Answer questions A & B in the comments.
      */
 
+    //run games method
+    public String runGames(int tries, boolean change) {
+        //accepts number of tries as input and change door choice as input
+        Random r = new Random();
+        int bound = DOORS_SIZE+1;
+        //for each try,
+        for(int i = 0; i < tries; i++) {
+            //randomly assign the winning door
+            int winningDoor = r.nextInt(1, bound);
+            //assign the two other doors a goat
+            int[] goatDoors;
+            switch(winningDoor){
+                case 1 -> goatDoors = new int[]{2, 3};
+                case 2 -> goatDoors = new int[]{1, 3};
+                case 3 -> goatDoors = new int[]{1, 2};
+            }
+            //randomly pick a door as our choice
+            int contestantChoice = r.nextInt(1, bound);
+            //host shows one of the non winning doors
+            //if change is true, change to another door
+            if(change) {
 
+            }
+            //else do not change doors
+            //if winner = choice, increment wins
+            if(winningDoor == contestantChoice) {
+                setWins(getWins() + 1);
+            }
+        }
+        String result;
+        //return a string telling user various statistics about the games
+        if(change){
+            result = "In 10000 Games, you changed your door each time and won " + getWins() + " times.";
+        }
+        else{
+            result = "In 10000 Games, you did not change your door and won " + getWins() + " times.";
+        }
+        return result;
+    }
 
+    public MonteHall(){
+        setWins(0);
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    private int wins;
+
+    private static final int DOORS_SIZE = 3;
 
 }
--- src/Mini_Coding_Projects/Monty_Hall_DealNoDeal/MonteHallTester.java
+++ src/Mini_Coding_Projects/Monty_Hall_DealNoDeal/MonteHallTester.java
@@ -1,4 +1,14 @@
 package Mini_Coding_Projects.Monty_Hall_DealNoDeal;
 
 public class MonteHallTester {
+    public static void main(String[] args) {
+        MonteHall monteHall = new MonteHall();
+        /*
+        Simple Probability of random choice is 1/3.
+
+        Probability of picking the winning door after seeing a dud is
+         */
+        System.out.println(monteHall.runGames(10000, true));
+        System.out.println(monteHall.runGames(10000, false));
+    }
 }
