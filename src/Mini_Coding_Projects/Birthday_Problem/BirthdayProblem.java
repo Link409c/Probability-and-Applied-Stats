1df2f99ec57d13d379b24921f4624218def63efb
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-11-01 16:52:22 -0400

    Reevaluated implementation of book example for calculations. Debugging led to the realization the signum of the final big integer value was zero due to division operations, which was returning zero as a result. changing division to double values with big integer methods led to the correct calculation.

--- src/Mini_Coding_Projects/Birthday_Problem/BirthdayProblem.java
+++ src/Mini_Coding_Projects/Birthday_Problem/BirthdayProblem.java
@@ -20,19 +20,26 @@ public class BirthdayProblem {
     public double calcBdayProblem(){
         //get sample size
         int numOfPeople = getPeople().size();
-        double result;
+        BigInteger days = BigInteger.valueOf(365);
         //create stats library object
         StatsLibrary calcBirthdays = new StatsLibrary();
-        int days = 365;
-        //calculate total number of permutations of birthdays in the sample space
-        BigInteger bDayPermutations = calcBirthdays.findPermutations(days, numOfPeople);
-        //calculate total number of birthdays in the sample space
-        BigInteger bDays = BigInteger.valueOf((long) Math.pow(days, numOfPeople));
-        //divide the permutations by the total
-        BigInteger bigResult = bDayPermutations.divide(bDays);
-        //subtract the result from the total probability to get the compliment
-        result = 1 - bigResult.doubleValue();
-        return result;
+        //calculate the total number of points in the sample space
+        //365^n where n is the number of people
+        BigInteger totalPoints = days.pow(numOfPeople);
+        //calculate the number of sample points
+        //first person 365/365, second person 364/365, etc.
+        //365x364x363x...x365-n
+        int difference = days.intValue() - numOfPeople;
+        //get 365 factorial and 365 - n factorial
+        BigInteger numerator = calcBirthdays.calculateFactorial(days.intValue());
+        BigInteger denominator = calcBirthdays.calculateFactorial(difference);
+        //365x364x...x(365-n)x(365-n)! divided by (365-n)! leaves us with first (365-n+n) elements multiplied
+        //divide them to be left with 365x364x...x365-n
+        BigInteger samplePoints = numerator.divide(denominator);
+        //divide sample points by total points to get probability of nobody sharing a birthday
+        double noneSharing = samplePoints.doubleValue() / totalPoints.doubleValue();
+        //get the compliment to determine probability that at least two people share a birthday.
+        return calcBirthdays.complimentOfEvent(noneSharing);
     }
 
     public BirthdayProblem(ArrayList<Person> thePeople){
