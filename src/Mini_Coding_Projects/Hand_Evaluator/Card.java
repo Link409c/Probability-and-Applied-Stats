100c8cf16e6d9f65d4b5191d5bb99cf91eb4ca11
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-11-12 23:29:33 -0500

    removed implementation of comparable interface - handled with custom comparator for card class.

--- src/Mini_Coding_Projects/Hand_Evaluator/Card.java
+++ src/Mini_Coding_Projects/Hand_Evaluator/Card.java
@@ -4,17 +4,7 @@ package Mini_Coding_Projects.Hand_Evaluator;
 /**
  * A Data Container emulating a playing card. Has an enum Suit and a value.
  */
-public class Card implements Comparable<Card> {
-
-	/**
-	 * compares card values.
-	 * @param o the object to be compared.
-	 * @return 1 if this is greater, 0 if equal, -1 if this is less.
-	 */
-	@Override
-	public int compareTo(Card o) {
-		return Integer.compare(this.getValue(), o.getValue());
-	}
+public class Card {
 
 	public Card(Suit theSuit, int theValue) {
 		setSuit(theSuit);
@@ -42,6 +32,5 @@ public class Card implements Comparable<Card> {
 	 * Face cards are represented by the numbers 11-13 (J,Q,K).
 	 */
 	private int value;
-	
-	//test
+
 }
