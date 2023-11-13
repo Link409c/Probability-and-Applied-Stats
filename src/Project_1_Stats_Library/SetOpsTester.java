5951b4fb6dc8c20262a361386278b2623a32d6c6
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-11-13 00:15:43 -0500

    created tester for set operations

---
+++ src/Project_1_Stats_Library/SetOpsTester.java
@@ -0,0 +1,9 @@
+package Project_1_Stats_Library;
+
+public class SetOpsTester {
+    public static void main(String[] args){
+        //test union
+        //test intersection
+        //test compliment
+    }
+}
