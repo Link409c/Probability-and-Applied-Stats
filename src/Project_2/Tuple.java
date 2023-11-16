e84f7b8e13645932f0f96bd221a915af35d5df3f
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-11-15 20:34:25 -0500

    created generic data container to use with plotter, salter, and smoother.

---
+++ src/Project_2/Tuple.java
@@ -0,0 +1,39 @@
+package Project_2;
+
+/**
+ * a data container for related pairs of values.
+ * @param <E>
+ */
+public class Tuple<E> {
+
+    public Tuple(E input, E output){
+        setInput(input);
+        setOutput(output);
+    }
+
+    public E getInput() {
+        return input;
+    }
+
+    public void setInput(E input) {
+        this.input = input;
+    }
+
+    public E getOutput() {
+        return output;
+    }
+
+    public void setOutput(E output) {
+        this.output = output;
+    }
+
+    /**
+     * some input value to a function.
+     */
+    private E input;
+    /**
+     * the output value of a function using the associated input.
+     */
+    private E output;
+
+}
