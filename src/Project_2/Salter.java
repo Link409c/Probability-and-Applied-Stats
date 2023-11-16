2d39622da8ead1dc1efa9d41b6ac906c1a645363
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-11-15 20:30:47 -0500

    added tuple object to hold pairs of points for output.

--- src/Project_2/Salter.java
+++ src/Project_2/Salter.java
@@ -1,19 +1,19 @@
 package Project_2;
 
+import java.util.ArrayList;
+
 /**
  * Salter takes a csv file input and "salts" the data by adding or subtracting from
  * each value to change the plot, effectively scattering the information on an observable graph.
- * Author: Christian Simpson
- * Version: 10/24/23
 **/
 
 public class Salter{
   //method to input the data
-  //method to dalt each data point
+  //method to salt each data point
   //method to output the data
   
   //the plot data created with salt
-  private double[] plotData;
+  private ArrayList<Tuple<Double>> saltedPoints;
 
   //be able to configure the range of salt values
   //loop thru y values and +/- from each
