2d78f9f013d7a6641f7fb10255c7f7d77ff2e7c1
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-11-15 20:28:58 -0500

    created tester for plot / salt / smooth programs.

---
+++ src/Project_2/PlotSaltSmoothTester.java
@@ -0,0 +1,15 @@
+package Project_2;
+
+/**
+ * tester class for the plotter, salter, and smoother programs.
+ */
+public class PlotSaltSmoothTester {
+    //plot some values
+    //export
+    //import
+    //salt the values
+    //export
+    //import
+    //smooth the values
+    //export
+}
