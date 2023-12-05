d0a91019db4944b5ed239dbb242ec01d13e9a7c6
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-12-05 17:10:12 -0500

    Plotter: successful run of plot and export methods.

--- src/Project_2/Plot_Salt_Smooth/PlotSaltSmoothTester.java
+++ src/Project_2/Plot_Salt_Smooth/PlotSaltSmoothTester.java
@@ -1,15 +1,35 @@
 package Project_2.Plot_Salt_Smooth;
 
+import java.io.IOException;
+import java.util.Random;
+
 /**
  * tester class for the plotter, salter, and smoother programs.
  */
 public class PlotSaltSmoothTester {
-    //plot some values
-    //export
-    //import
-    //salt the values
-    //export
-    //import
-    //smooth the values
-    //export
+    public static void main(String[] args) {
+        Plotter plotter = new Plotter();
+        Random r = new Random();
+        //populate the list with random doubles
+        for(int i = 0; i < 51; i++){
+            double d = r.nextDouble(1, 5);
+            plotter.getInputs().add(d);
+        }
+        //plot some values
+        plotter.plotFunction();
+        //export
+        String fileName = "plotterTest";
+        String header = "Input,Output";
+        try {
+            System.out.println(plotter.exportObjects(fileName, header));
+        }catch(IOException i){
+            i.printStackTrace();
+        }
+        //import
+        //salt the values
+        //export
+        //import
+        //smooth the values
+        //export
+    }
 }
