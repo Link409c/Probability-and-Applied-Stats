5865578c2a8a60422c4d618f2ae844c6a87acc27
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-12-07 00:27:58 -0500

    successfully tested all methods. created output files.

--- src/Project_2_Final/Plot_Salt_Smooth/PlotSaltSmoothTester.java
+++ src/Project_2_Final/Plot_Salt_Smooth/PlotSaltSmoothTester.java
@@ -33,14 +33,44 @@ public class PlotSaltSmoothTester {
         }catch(IOException i){
             i.printStackTrace();
         }
+        //check data was imported correctly
+        /*
         System.out.println("Import Data from plotter output file:");
         for(Tuple<Double> t : salter.getSaltedPoints()){
             System.out.println(t.getInput() + ", " + t.getOutput());
         }
+        */
         //salt the values
+        salter.salt();
         //export
+        fileName = "saltTest";
+        try {
+            System.out.println(salter.exportObjects(fileName, header));
+        }catch(IOException i){
+            i.printStackTrace();
+        }
         //import from salter output file
+        Smoother smoother = new Smoother();
+        fileName = fileName.concat(".csv");
+        try{
+            smoother.importObjects(fileName);
+        }catch(IOException i){
+            i.printStackTrace();
+        }
+        int theWindow = 10;
         //smooth the values
+        smoother.smooth(theWindow);
+        //check smoothed values
+        /*System.out.println("Smoothing points from salter output file:");
+        for(Tuple<Double> t : smoother.getSmoothedPoints()){
+            System.out.println(t.getInput() + ", " + t.getOutput());
+        }*/
         //export
+        fileName = "smoothTest";
+        try{
+            System.out.println(smoother.exportObjects(fileName, header));
+        }catch(IOException i){
+            i.printStackTrace();
+        }
     }
 }
