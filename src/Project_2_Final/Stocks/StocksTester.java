e44bf25c64577d458fbb5a2e5bb8abff7509d361
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-12-07 00:32:47 -0500

    successfully tested imports.

--- src/Project_2_Final/Stocks/StocksTester.java
+++ src/Project_2_Final/Stocks/StocksTester.java
@@ -1,9 +1,20 @@
 package Project_2_Final.Stocks;
 
+import java.io.IOException;
+
 public class StocksTester {
 
     public static void main(String[] args){
         //test import
+        StockAnalyzer stockAnalyzer = new StockAnalyzer();
+        try{
+            stockAnalyzer.importObjects("MSFT.csv");
+        }catch (IOException i){
+            i.printStackTrace();
+        }
+        StockDay aDay = stockAnalyzer.getDaysData().get(0);
+        System.out.println("Date | Open | High | Low | Close | Adj Close | Volume");
+        System.out.println(aDay.dailyValuesToString());
         //test buying and selling
         //test export
     }
