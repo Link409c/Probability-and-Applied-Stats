4195f5fbe6dff9d351576dd58da699d2ee70b562
CSimpson <cdsimpsonnavy2010@yahoo.com>
2023-12-18 21:42:11 -0500

    created ProbabilityOperations interface, designed to restructure the statslibrary class.

---
+++ src/Miscellaneous/Interfaces/ProbabilityOperations.java
@@ -0,0 +1,14 @@
+package Miscellaneous.Interfaces;
+
+public interface ProbabilityOperations {
+
+    //a method to count the number of unique objects in a list.
+
+    //a method to assign simple probabilities to the unique objects in a list.
+
+    //methods to create unions and intersections of objects based on their supertypes.
+
+    //methods to check lists of objects against the axioms of probability.
+
+    //methods to return strings informing the user of the probabilities of objects in a list.
+}
