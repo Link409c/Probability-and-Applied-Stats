0cfbd6a9a8cd5cbbc4206697f652afae78070c79
CSimpson <cdsimpsonnavy2010@yahoo.com>
2024-01-08 22:36:47 -0500

    added comments for use of different methods. added central tendency methods and calculateProbability method. added javadoc.

--- src/Miscellaneous/Interfaces/ProbabilityOperations.java
+++ src/Miscellaneous/Interfaces/ProbabilityOperations.java
@@ -1,5 +1,11 @@
 package Miscellaneous.Interfaces;
 
+/**
+ * The ProbabilityOperations interface is a generics interface that should be used
+ * when constructing classes that use some probability calculations in them. This
+ * could be the end goal of the class (calculating probability using a specific
+ * formula or theorem) or serve the goal of a class or program.
+ */
 public interface ProbabilityOperations<E> {
 
     //a method to count the number of unique objects in a list.
@@ -9,7 +15,10 @@ public interface ProbabilityOperations<E> {
     E assignObjectProbabilities(E aList);
 
     //methods to create unions and intersections of objects based on their supertypes.
+
+    //unite is useful if an object has multiple data structures or lists of objects.
     E unite(E theLists);
+    //intersect is useful when considering comparable objects.
     E intersect(E theLists);
 
     //methods to check lists of objects against the axioms of probability.
@@ -17,4 +26,13 @@ public interface ProbabilityOperations<E> {
 
     //methods to return strings informing the user of the probabilities of objects in a list.
     String listProbabilities(E aList);
+
+    //method to calculate specific probabilities for the objects within a data structure.
+    double calculateProbability(E aList);
+
+    //methods to calculate central tendency for a list of objects
+    E calcMean(E aList);
+    E calcMode(E aList);
+    E calcMedian(E aList);
+    E calcStandardDev(E aList);
 }
